91acf073ea00c3d7136f7e5bd5a9b78e505f0b51
yoyobara <yotambarak6@gmail.com>
2022-12-30 19:02:59 +0200

    completed c++ socket implementation (not tested) and added gitignore

--- include/cpp_socket.h
+++ include/cpp_socket.h
@@ -1,5 +1,53 @@
 #pragma once
 
-#include <initializer_list>
+#include <cstdint>
+#include <exception>
+#include <string>
+#include <string_view>
+#include <sys/socket.h>
 
-double avg(std::initializer_list<double> lis);
+/**
+ * socket connection exception
+ */
+class socket_error : public std::exception
+{
+	const char* msg;
+
+	public:
+		socket_error(const char* message);
+		const char * what();
+};
+
+
+/**
+ * an abstraction of C sockets to use with C++
+ * 
+ */
+class Socket
+{
+	public:
+		/* create new socket */
+		Socket();
+		
+		/* destructs a socket */
+		virtual ~Socket();
+
+		/* binds server to ip and port */
+		void bind(uint16_t port) const;
+
+		/* listen to connections */
+		void listen(int backlog) const;
+
+		/* accept connection */
+		Socket accept() const;
+
+		/* connect to server */
+		void connect(const std::string& ip_address, uint16_t port) const; 
+
+	protected:
+		/* socket file descriptor */
+		int m_sockfd;
+
+		/* construct from existing socket */
+		Socket(int fd);
+};
--- src/cpp_socket.cpp
+++ src/cpp_socket.cpp
@@ -1,10 +1,67 @@
 #include "cpp_socket.h"
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <cstring>
 
-double avg(std::initializer_list<double> lis)
+// socket error implementation
+socket_error::socket_error(const char* message) : msg(message){}
+const char* socket_error::what(){ return msg;}
+
+Socket::Socket()
 {
-	double sum = 0;
-	for (double d : lis)
-		sum += d;
+	m_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (m_sockfd < 0) throw socket_error("socket creation error");
+}
 
-	return sum / lis.size();
+Socket::Socket(int fd) : m_sockfd(fd) {}
+
+Socket::~Socket()
+{
+	close(m_sockfd);
+}
+
+void Socket::bind(uint16_t port) const
+{
+	struct sockaddr_in addr;
+	memset(&addr, 0, sizeof addr);
+	addr.sin_port = htons(port);
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = INADDR_ANY;
+
+	int res = ::bind(m_sockfd, (struct sockaddr*)&addr, sizeof(struct sockaddr));
+	if (res < 0)
+		throw socket_error("socket bind error");
+}
+
+void Socket::listen(int backlog) const
+{
+	int res = ::listen(m_sockfd, backlog);
+	if (res < 0)
+		throw socket_error("socket listen error");
+}
+
+Socket Socket::accept() const
+{
+	struct sockaddr_in client_address;
+	socklen_t conn_length = sizeof(client_address);
+
+	int conn = ::accept(m_sockfd, (struct sockaddr*)&client_address, &conn_length);
+	if (conn < 0) throw socket_error("accept error");
+
+	return Socket(conn);
+}
+
+void Socket::connect(const std::string& ip_address, uint16_t port) const
+{
+	struct sockaddr_in addr;
+	memset(&addr, 0, sizeof addr);
+	addr.sin_port = htons(port);
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = inet_addr(ip_address.c_str());
+
+	int result = ::connect(m_sockfd, (struct sockaddr*)&addr, sizeof addr);
+	if (result < 0)
+		throw socket_error("connecting error");
 }
