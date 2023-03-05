320afb43be797a1d83a2b911f7302be360b0facb
yoyobara <yotambarak6@gmail.com>
2023-03-05 19:46:42 +0200

    continued threaded sever

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,58 @@
+#include <cstdlib>
 #include <iostream>
 #include <vector>
 #include <thread>
+#include <csignal>
+#include <atomic>
 
 #include "cpp_socket.h"
+#include "communication_protocol.h"
 #include "table.h"
 
-/**
- * a thread running function which is in charge of handling a simple client
- */
-void handle_client(Socket client)
+// a vector containing the currently running threads
+std::vector<std::thread> running_threads;
+std::atomic<bool> stop_program(false);
+
+class ClientThread
 {
-	client.close();
+	private:
+
+		std::thread m_thread;
+		Socket m_client;
+
+		void run()
+		{
+			// TODO run program
+		}
+
+		void stop()
+		{
+			// TODO close connections and cleanup
+			m_thread.join();
+		}
+
+	public:
+
+		ClientThread(Socket client) : m_client(client) {}
+
+		void start()
+		{
+			m_thread = std::thread(&ClientThread::run, this);
+		}
+};
+
+/**
+ * handle ctrl+c (sigint) to close and cleanup the threads
+ */
+void handle_sigint(int dummy)
+{
+	stop_program = true;
+
+	for (std::thread& t: running_threads) {
+		t.join();
+	}
+
+	exit(EXIT_SUCCESS);
 }
 
 int main()
@@ -19,8 +61,7 @@ int main()
 	server.bind(1337);
 	server.listen(2);
 
-	// a vector containing the currently running threads
-	std::vector<std::thread> running_threads;
+	signal(SIGINT, handle_sigint);
 
 	// repeatedly accept clients, handle them in seperate threads.
 	while (true)
@@ -28,6 +69,4 @@ int main()
 		Socket client = server.accept();
 		running_threads.push_back(std::thread(handle_client, client));
 	}
-
-	return EXIT_SUCCESS;
 }
--- src/sample_client.py
+++ src/sample_client.py
@@ -4,6 +4,6 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('127.0.0.1', 1337))
 
-s.send(b'helloworld')
+print(s.recv(2048))
 
 s.close()
