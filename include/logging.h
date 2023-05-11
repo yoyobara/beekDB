bb2ac4ec007b9b95f0c58edb4d800a2b563d67c2
yoyobara <yotambarak6@gmail.com>
2023-05-11 13:20:30 +0300

    wow

--- include/logging.h
+++ include/logging.h
@@ -4,19 +4,21 @@
 #include <spdlog/sinks/stdout_color_sinks.h>
 
 
-namespace logging
-{
-	using logger_ptr = const std::shared_ptr<spdlog::logger>;
+using logger_ptr = std::shared_ptr<spdlog::logger>;
 
-	// loggers available to use
+// loggers available to use
 
-	logger_ptr main_logger = spdlog::stdout_color_mt("main_logger");
-	logger_ptr network_logger = spdlog::stdout_color_mt("network_logger");
-	logger_ptr statement_logger = spdlog::stdout_color_mt("statement_logger");
-	logger_ptr server_logger = spdlog::stdout_color_mt("statement_logger");
-}
+extern logger_ptr main_logger;
+extern logger_ptr network_logger;
+extern logger_ptr statement_logger;
+extern logger_ptr server_logger;
+extern logger_ptr table_logger;
 
 inline void setup_logger()
 {
-	spdlog::set_default_logger(logging::main_logger);
+	main_logger = spdlog::stdout_color_mt("main_logger");
+	table_logger = spdlog::stdout_color_mt("table_logger");
+	network_logger = spdlog::stdout_color_mt("network_logger");
+	server_logger = spdlog::stdout_color_mt("server_logger");
+	statement_logger = spdlog::stdout_color_mt("statement_logger");
 }
