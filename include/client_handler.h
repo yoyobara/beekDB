4dbe81eef33409e0c149aab0ef35a91fa2bb8c39
yoyobara <yotambarak6@gmail.com>
2023-05-27 22:14:59 +0300

    private finalize method

--- include/client_handler.h
+++ include/client_handler.h
@@ -19,15 +19,15 @@ class ClientThread
 		void handle_create_statement(const hsql::CreateStatement* statement);
 		void handle_insert_statement(const hsql::InsertStatement* statement);
 
+        // cleanup ssl and stuff
+        void finalize();
+
 	public:
 		/* is false when the program should be terminated */
 		static std::atomic<bool> program_running;
 
 		ClientThread(SSL* client_ssl);
 		ClientThread(ClientThread&& cli);
-
-        // cleanup ssl and stuff
-        void finalize();
 
 		/* thread execution entry point */
 		void operator()();
